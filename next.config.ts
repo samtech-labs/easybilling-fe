bf9a05bb3d013cbed7c9cba25be02f9672750d97
Andrei Samoila <samoilaandrei76@gmail.com>
2025-12-21 13:27:20 +0200

    updatae config for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["10.211.55.5"]
+  allowedDevOrigins: ["10.211.55.5"],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
